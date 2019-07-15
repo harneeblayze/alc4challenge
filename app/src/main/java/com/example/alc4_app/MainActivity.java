c30a42f4b8e31a5df8d369192b79c8ac1266e8a6
Harny <orievictor123@gmail.com>
2019-07-15 18:03:06 +0100

    profile page remaining

---
+++ app/src/main/java/com/example/alc4_app/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.alc4_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button aboutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        aboutButton = findViewById(R.id.launch_about);
+
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebViewActivity.start(MainActivity.this);
+            }
+        });
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/alc4_app/WebViewActivity.java
@@ -0,0 +1,49 @@
+package com.example.alc4_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+
+        webView = findViewById(R.id.webView1);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed(); // Ignore SSL certificate errors
+            }
+        });
+
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(true);
+        webView.loadUrl("https://andela.com/alc/");
+    }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, WebViewActivity.class);
+        context.startActivity(starter);
+    }
+}
