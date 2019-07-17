adabc330bdfa4ca23e6446ada2e373478e25a0ae
Harny <harneeblayze@gmail.com>
2019-07-17 08:40:27 +0100

    final touches

--- app/src/main/java/com/example/alc4_app/MainActivity.java
+++ app/src/main/java/com/example/alc4_app/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.alc4_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button aboutButton;
+    Button myProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +23,35 @@ public class MainActivity extends AppCompatActivity {
 
         aboutButton = findViewById(R.id.launch_about);
 
+
         aboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 WebViewActivity.start(MainActivity.this);
+
+
+            }
+        });
+
+        myProfile = findViewById(R.id.launch_profile);
+
+        myProfile.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                /*Intent myProfile = new Intent(MainActivity.this, MyProfile.class );
+
+                startActivity(myProfile);
+                */
+
+                MyProfile.start(MainActivity.this);
+                Log.d("check", ">>>>>>>>>>>>>>");
+
             }
         });
     }
+
+
 
 
 
--- app/src/main/java/com/example/alc4_app/MyProfile.java
+++ app/src/main/java/com/example/alc4_app/MyProfile.java
@@ -2,6 +2,8 @@ package com.example.alc4_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MyProfile extends AppCompatActivity {
@@ -11,4 +13,10 @@ public class MyProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
     }
+
+    public static void start(Context context) {
+        Intent starter = new Intent(context, MyProfile.class);
+        context.startActivity(starter);
+    }
+
 }
